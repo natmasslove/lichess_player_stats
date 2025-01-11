65a28e81bcbcac85715b4224aa3cb3053d4a7962
Anatolii Maslov <amaslov78@gmail.com>
2025-01-11 09:34:38 +0300

    added get_games_headers method to LichessAPIHelper

--- helpers/lichess_api_helper.py
+++ helpers/lichess_api_helper.py
@@ -1,7 +1,74 @@
-from typing import Iterator
-import requests
+import io
+from typing import Optional, TypedDict
+from helpers.urllib3_helper import Urllib3Helper
+from dataclasses import dataclass
+
+from chess.pgn import read_game, read_headers
+
 
 BASE_URL = "https://lichess.org/"
+
+
+@dataclass
+class PGNGameHeader:
+    Event: Optional[str] = None
+    Site: Optional[str] = None
+    Date: Optional[str] = None
+    Round: Optional[str] = None
+
+    White: Optional[str] = None
+    Black: Optional[str] = None
+    Result: Optional[str] = None
+
+    UTCDate: Optional[str] = None
+    UTCTime: Optional[str] = None
+
+    Score: Optional[str] = None
+    WhiteElo: Optional[str] = None
+    BlackElo: Optional[str] = None
+    WhiteRatingDiff: Optional[str] = None
+    BlackRatingDiff: Optional[str] = None
+
+    Variant: Optional[str] = None
+    TimeControl: Optional[str] = None
+
+    ECO: Optional[str] = None
+    Opening: Optional[str] = None
+
+    PlayerTitle: Optional[str] = None
+    OpponentTitle: Optional[str] = None
+    Termination: Optional[str] = None
+
+
+class GetGamesParams(TypedDict):
+    # >= 1356998400070 Download games played since this timestamp. Defaults to account creation date
+    since: Optional[int]
+
+    # >= 1356998400070 Download games played until this timestamp. Defaults to now.
+    until: Optional[int]
+
+    # >= 1 How many games to download. Leave empty to download all games.
+    max: Optional[int]
+
+    # [Filter] Only rated (true) or casual (false) games
+    rated: Optional[bool]
+
+    # Default: null
+    # Enum: "ultraBullet" "bullet" "blitz" "rapid" "classical" "correspondence" "chess960" "crazyhouse" "antichess" "atomic" "horde" "kingOfTheHill" "racingKings" "threeCheck"
+    # [Filter] Only games in these speeds or variants. Multiple perf types can be specified, separated by a comma. Example: blitz,rapid,classical
+    perfType: Optional[str]
+
+    # Default: true
+    # Include the PGN moves.
+    moves: Optional[bool]
+
+    # Default: false
+    # Include the full PGN within the JSON response, in a pgn field. The response type must be set to application/x-ndjson by the request Accept header.
+    pgnInJson: Optional[bool]
+
+    # Default: false
+    # Include the opening name. Example: [Opening "King's Gambit Accepted, King's Knight Gambit"]
+    opening: Optional[bool]
 
 
 class LichessAPIHelper(Exception):
@@ -9,35 +76,28 @@ class LichessAPIHelper(Exception):
 
 
 class LichessAPIHelper:
-    def __init__(self, lichess_token: str):
-        self.lichess_token = lichess_token
+    def __init__(self, lichess_api_token: str):
+        self.lichess_api_token = lichess_api_token
         self.base_url = BASE_URL
+        self.req_helper = Urllib3Helper(BASE_URL)
 
-    def _exec_request(
-        self, api_url: str, params: dict = None, additional_headers: dict = None
-    ) -> requests.Response:
-        headers = {
-            "Authorization": f"Bearer {self.lichess_token}",
-        }
-        if additional_headers:
-            headers.update(additional_headers)
-
-        url = self.base_url + api_url
-        response = requests.get(url, headers=headers, params=params)
-        if response.status_code == 200:
-            return response
-        else:
-            raise LichessAPIHelper(
-                f"Failed API request: {api_url}. Code/Error: {response.status_code}/{response.reason}"
-            )
-
-    def exec_request_json(self, api_url: str, params: dict = None) -> dict:
-        response = self._exec_request(api_url, params)
-        return response.json()
-
-    def exec_request_ndjson(self, api_url: str, params: dict = None) -> Iterator[bytes]:
-        additional_headers = {"Accept": "application/x-ndjson"}
-        response = self._exec_request(
-            api_url, params, additional_headers=additional_headers
+    def get_games_headers(
+        self, username: str, params: GetGamesParams
+    ) -> list[PGNGameHeader]:
+        api_path = f"api/games/user/{username}"
+        pgn_response = self.req_helper.get(
+            api_path=api_path, params=params, token=self.lichess_api_token
         )
-        return response.iter_lines()
+
+        # convert pgn_response to a stream readable by read_game
+        pgn_stream = io.StringIO(pgn_response)
+
+        output = []
+        while True:
+            game = read_game(pgn_stream)
+            if game is None:
+                break
+
+            output.append(PGNGameHeader(**dict(game.headers)))
+
+        return output
--- helpers/urllib3_helper.py
+++ helpers/urllib3_helper.py
@@ -8,7 +8,7 @@ class Urllib3Exception(Exception):
 
 
 class Urllib3Helper:
-    def __init__(self, base_url) -> None:
+    def __init__(self, base_url: str) -> None:
         """_summary_
 
         Args:
@@ -17,13 +17,13 @@ class Urllib3Helper:
         self.http = urllib3.PoolManager()
         self.base_url = base_url
 
-    def _prep_headers(self, token=None):
+    def _prep_headers(self, token: str | None = None) -> dict:
         headers = {"Content-Type": "application/json"}
         if token:
             headers["Authorization"] = f"Bearer {token}"
         return headers
 
-    def _get_api_url(self, api_path, data=None):
+    def _get_api_url(self, api_path: str) -> str:
         """
         Construct the full API URL from the base URL and API path.
         """
@@ -31,7 +31,9 @@ class Urllib3Helper:
         url = urljoin(self.base_url, api_path)
         return url
 
-    def get(self, api_path, params=None, token=None):
+    def get(
+        self, api_path: str, params: dict | None = None, token: str | None = None
+    ) -> str:
         """
         Makes a GET request to the specified API path with optional query parameters.
 
@@ -67,40 +69,3 @@ class Urllib3Helper:
                 )
         except Exception as e:
             raise Urllib3Exception(f"An error occurred during GET: {str(e)}")
-
-    def post(self, api_path, params=None, token=None):
-        """
-        Makes a POST request to the specified API path with a JSON body.
-
-        Args:
-            api_path (str): API path to append to the base URL.
-            params (dict, optional): Data to send with the POST request. Defaults to None.
-            token (str, optional): Authorization token. Defaults to None.
-
-        Returns:
-            dict: Decoded JSON response.
-
-        Raises:
-            Urllib3Exception: If the request fails or an error occurs.
-        """
-        url = self._get_api_url(api_path)
-        print(f"{url = }")
-        headers = self._prep_headers(token)
-        body = json.dumps(params) if params else None
-
-        try:
-            response = self.http.request(
-                method="POST",
-                url=url,
-                body=body,
-                headers=headers,
-            )
-
-            if response.status == 200:
-                return response.data.decode("utf-8")
-            else:
-                raise Urllib3Exception(
-                    f"POST request failed: {response.status} {response.data.decode('utf-8')}"
-                )
-        except Exception as e:
-            raise Urllib3Exception(f"An error occurred during POST: {str(e)}")
